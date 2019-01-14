99aceead2c7d26bf3559bfccd958f7fa375066e4
Александр Тетенков <tetenkov@tecon.ru>
2019-01-14 12:26:13 +0300

    added allocate and free for tethered list allocator logic

--- allocation.h
+++ allocation.h
@@ -12,28 +12,28 @@
 
 
 #define BATCH_SIZE 4
-#define BATCH_COUNT 32
+#define BATCH_COUNT 4
 
 
 
 typedef struct AllocatorUnit
 {
-	struct AllocatorUnit* start;	//Pointer to the start of blocks chain
-	struct AllocatorUnit* end;		//Pointer to the end of blocks chain
-	uint8_t data[BATCH_SIZE];
-	uint8_t used;	//Data in usage flag
+	struct AllocatorUnit* next;	//Pointer to the start of blocks chain
+	uint8_t data[BATCH_SIZE - sizeof(char*)];
 }AllocatorUnit;
 
 
 typedef struct AllocatorPool
 {
+	AllocatorUnit* start;
 	AllocatorUnit* head;
+	AllocatorUnit mem[BATCH_COUNT];
 } AllocatorPool;
 
-void* allocatorInit(AllocatorUnit* unit);
-void* allocBatch(AllocatorUnit* unit);
-int freeBatch(AllocatorUnit* unit, void* batch);
-AllocatorUnit* batchToUnit(void* batch);
-void printUnits(AllocatorUnit* unit, uint16_t place, uint16_t count);
+void allocatorInit(AllocatorPool* pool);
+void* allocBatch(AllocatorPool* pool);
+int freeBatch(AllocatorPool* pool, void* batch);
+void printUnits(AllocatorPool* pool, uint32_t place, uint32_t count);
+void printTetheredList(AllocatorPool* pool);
 
 #endif /* ALLOCATION_H_ */
